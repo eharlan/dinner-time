a0b66bd806c0a5e7fa21df75ce337949b4811654
Erik Harlan <harlanes14@gmail.com>
2021-11-19 14:33:15 -0700

    Update API to Spring Framework with basic setup

---
+++ src/test/java/com/eharlan/dinnertime/app/DinnertimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eharlan.dinnertime.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnertimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
